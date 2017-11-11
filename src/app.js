668878eb15a4494c1de43dd5c8f9cb8d98930f08
liebeskind <daniel.liebeskind@gmail.com>
2017-11-11 18:36:41 -0500

    Adds firebase to project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common/index';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC96A8rI1bqxqLtFCdbqsRGbvGDEOCmI7Y',
+      authDomain: 'authexample-67260.firebaseapp.com',
+      databaseURL: 'https://authexample-67260.firebaseio.com',
+      projectId: 'authexample-67260',
+      storageBucket: 'authexample-67260.appspot.com',
+      messagingSenderId: '403863614896'
+    });
+  }
+
   render() {
     return (
       <View>
