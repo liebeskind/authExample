49b46da1243de004ca56600d68af09a21f091f60
liebeskind <daniel.liebeskind@gmail.com>
2017-11-28 11:27:07 -0500

    Adds spinner and styling

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   state= { 
     email: '', 
     password: '',
-    error: ''
+    error: '',
+    loading: false
   };
 
   onButtonPress() {
     const { email, password } = this.state; //destructuring
+    
+    this.setState({ error: '', loading: true }); //Need strong UI feedback in mobile - bad internet.
+
     firebase.auth().signInWithEmailAndPassword(email, password)
     .catch(() => { //If this request fails, enter the fat arrow function.
-      firebase.auth().createUserWithEmailAndPasswor(email, password)
+      firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(() => {
         this.setState({ error: 'Authentication Failed.' });
       });
     });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button 
+        onPress={this.onButtonPress.bind(this)} //Called later and want access to 'this'.
+      >
+        Log in
+      </Button>
+    );
   }
 
   render() {
@@ -47,11 +65,7 @@ class LoginForm extends Component {
         </Text>
         
         <CardSection>
-          <Button 
-            onPress={this.onButtonPress.bind(this)} //Called later and want access to 'this'.
-          >
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
