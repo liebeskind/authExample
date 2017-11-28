23c02f888b6986cb4c301ef6310a0e58f638648f
liebeskind <daniel.liebeskind@gmail.com>
2017-11-28 12:12:57 -0500

    Adds different rendering depending on whether user is signed in or not

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common/index';
+import { Header, Button, Spinner } from './components/common/index';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC96A8rI1bqxqLtFCdbqsRGbvGDEOCmI7Y',
@@ -14,13 +16,32 @@ class App extends Component {
       storageBucket: 'authexample-67260.appspot.com',
       messagingSenderId: '403863614896'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
