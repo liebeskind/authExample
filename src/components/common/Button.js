d5076f7dae6c520f1dd27bdbe9043f509f06fed7
liebeskind <daniel.liebeskind@gmail.com>
2017-11-28 13:19:19 -0500

    Finalize app

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,6 +3,7 @@ import { Text, TouchableOpacity } from 'react-native';
 
 const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
+  
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={textStyle}>
