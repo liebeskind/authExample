70d832c327547f697096c764df449d81fbbf89bc
liebeskind <daniel.liebeskind@gmail.com>
2017-11-28 11:01:19 -0500

    Full email login / sign up flow implemented

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state= { 
     email: '', 
-    password: '' 
+    password: '',
+    error: ''
   };
 
   onButtonPress() {
     const { email, password } = this.state; //destructuring
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => { //If this request fails, enter the fat arrow function.
+      firebase.auth().createUserWithEmailAndPasswor(email, password)
+      .catch(() => {
+        this.setState({ error: 'Authentication Failed.' });
+      });
+    });
   }
 
   render() {
@@ -34,6 +42,9 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         
         <CardSection>
           <Button 
@@ -46,5 +57,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
